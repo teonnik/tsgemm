dd52b85fde0d7b2dfe35ed010919aeb4b96a6856
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-02-04 10:52:25 +0100

    Use a MPI executor

--- apps/v2.cpp
+++ apps/v2.cpp
@@ -7,6 +7,9 @@
 
 #include <hpx/dataflow.hpp>
 #include <hpx/hpx.hpp>
+#include <hpx/include/resource_partitioner.hpp>
+#include <hpx/include/threads.hpp>
+#include <hpx/runtime/threads/executors/pool_executor.hpp>
 
 #include <algorithm>
 #include <chrono>
@@ -34,6 +37,12 @@ void schedule_local_gemm(tsgemm::seg_dim m_dim, tsgemm::seg_dim n_dim,
                          std::vector<hpx::shared_future<void>> &cini_futures) {
   // Futures for all tiles in column major order.
 
+  using hpx::dataflow;
+  using hpx::future;
+  using hpx::make_ready_future;
+  using hpx::util::annotated_function;
+  using std::move;
+  using tsgemm::gemm;
   using tsgemm::index_map;
 
   int lda = k_dim.len;
@@ -49,7 +58,7 @@ void schedule_local_gemm(tsgemm::seg_dim m_dim, tsgemm::seg_dim n_dim,
       int len_n = n_dim.seg_len(j);
       int c_offset = index_map(m_el_idx, n_el_idx, ldc);
       scalar *c_ptr = c_buffer.data() + c_offset;
-      hpx::future<void> cini_fut = hpx::make_ready_future();
+      future<void> cini_fut = make_ready_future();
 
       for (int k = 0; k < k_dim.num_seg(); ++k) {
         int k_el_idx = k_dim.el_index(k);
@@ -59,27 +68,30 @@ void schedule_local_gemm(tsgemm::seg_dim m_dim, tsgemm::seg_dim n_dim,
         scalar const *a_ptr = a_buffer.data() + a_offset;
         scalar const *b_ptr = b_buffer.data() + b_offset;
 
-        cini_fut =
-            hpx::dataflow(hpx::util::unwrapping(hpx::util::annotated_function(
-                              tsgemm::gemm<scalar>, "gemm")),
-                          len_m, len_n, len_k, scalar(1), a_ptr, lda, b_ptr,
-                          ldb, scalar(1), c_ptr, ldc, cini_fut);
+        auto gemm_func = annotated_function(
+            [=](auto &&) {
+              gemm(len_m, len_n, len_k, scalar(1), a_ptr, lda, b_ptr, ldb,
+                   scalar(1), c_ptr, ldc);
+            },
+            "gemm");
+        cini_fut = dataflow(gemm_func, cini_fut);
       }
-      cini_futures.push_back(std::move(cini_fut));
+      cini_futures.push_back(move(cini_fut));
     }
   }
 }
 
 template <typename scalar>
 void schedule_offload_and_send(
-    MPI_Comm comm_cart, tsgemm::c_dim const &rows_dim,
-    tsgemm::c_dim const &cols_dim, std::vector<scalar> const &cini_buffer,
-    std::vector<scalar> &send_buffer,
+    hpx::threads::executors::pool_executor const &executor, MPI_Comm comm_cart,
+    tsgemm::c_dim const &rows_dim, tsgemm::c_dim const &cols_dim,
+    std::vector<scalar> const &cini_buffer, std::vector<scalar> &send_buffer,
     std::vector<hpx::shared_future<void>> &gemm_futures,
     std::vector<hpx::future<void>> &comm_futures) noexcept {
+  using hpx::dataflow;
   using hpx::util::annotated_function;
-  using hpx::util::unwrapping;
   using hpx::util::yield_while;
+  using std::move;
   using tsgemm::accumulate;
   using tsgemm::index_map;
   using tsgemm::iterate_pieces;
@@ -118,39 +130,41 @@ void schedule_offload_and_send(
     int &tag = tags[dest_rank];
 
     // capture by value to avoid segfault due to lifetime issues
-    auto offload_and_send_f = [=] {
-      // offload
-      accumulate<scalar, 0>(prlen, pclen, cini_ptr, cini_ld, send_ptr, send_ld);
+    auto offload_and_send_f = annotated_function(
+        [=](auto &&) {
+          // offload
+          accumulate<scalar, 0>(prlen, pclen, cini_ptr, cini_ld, send_ptr,
+                                send_ld);
 
-      // send
-      MPI_Request req;
-      {
-        //          std::lock_guard<hpx::lcos::local::mutex> lk(mt);
-        MPI_Isend(send_ptr, num_elems, tsgemm::get_mpi_type<scalar>(),
-                  dest_rank, tag, comm_cart, &req);
-      }
+          // send
+          MPI_Request req;
+          {
+            //          std::lock_guard<hpx::lcos::local::mutex> lk(mt);
+            MPI_Isend(send_ptr, num_elems, tsgemm::get_mpi_type<scalar>(),
+                      dest_rank, tag, comm_cart, &req);
+          }
 
-      // yield if not sent yet
-      yield_while([&req] {
-        int flag;
-        MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
-        return flag == 0;
-      });
+          // yield if not sent yet
+          yield_while([&req] {
+            int flag;
+            MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
+            return flag == 0;
+          });
 
-      // manual yield
-      //      int flag = 0;
-      //      while (flag == 0) {
-      //        MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
-      //        hpx::this_thread::yield();
-      //      }
-    };
+          // manual yield
+          //      int flag = 0;
+          //      while (flag == 0) {
+          //        MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
+          //        hpx::this_thread::yield();
+          //      }
+        },
+        "offload/send");
 
     // wait on the gemm for the tile
-    auto offload_fut = hpx::dataflow(
-        unwrapping(annotated_function(offload_and_send_f, "offload/send")),
-        gemm_futures[tidx]);
+    auto offload_fut =
+        dataflow(executor, offload_and_send_f, gemm_futures[tidx]);
 
-    comm_futures.push_back(std::move(offload_fut));
+    comm_futures.push_back(move(offload_fut));
     ++tag;
     snd_offset += num_elems;
   };
@@ -161,12 +175,14 @@ void schedule_offload_and_send(
 
 template <typename scalar>
 void schedule_recv_and_load(
-    MPI_Comm comm_cart, tsgemm::c_dim const &rows_dim,
-    tsgemm::c_dim const &cols_dim, std::vector<scalar> &cfin_buffer,
-    std::vector<scalar> &recv_buffer,
+    hpx::threads::executors::pool_executor const &executor, MPI_Comm comm_cart,
+    tsgemm::c_dim const &rows_dim, tsgemm::c_dim const &cols_dim,
+    std::vector<scalar> &cfin_buffer, std::vector<scalar> &recv_buffer,
     std::vector<hpx::future<void>> &comm_futures) noexcept {
 
+  using hpx::dataflow;
   using hpx::util::annotated_function;
+  using std::move;
   using tsgemm::accumulate;
   using tsgemm::index_map;
   using tsgemm::iterate_pieces;
@@ -189,42 +205,44 @@ void schedule_recv_and_load(
     scalar *cfin_ptr = cfin_buffer.data() + cfin_offset;
     scalar *recv_ptr = recv_buffer.data() + rcv_offset;
 
-    auto recv_and_load_func = [=] {
-      // recv
-      auto recv_reqs = std::make_unique<MPI_Request[]>(num_procs);
-      {
-        //        std::lock_guard<hpx::lcos::local::mutex> lk(mt);
-        for (int src_rank = 0; src_rank < num_procs; ++src_rank) {
-          auto &req = recv_reqs[src_rank];
-          MPI_Irecv(recv_ptr + src_rank * num_elems, num_elems,
-                    tsgemm::get_mpi_type<scalar>(), src_rank, tag, comm_cart,
-                    &req);
-        }
-      }
+    auto recv_and_load_func = annotated_function(
+        [=] {
+          // recv
+          auto recv_reqs = std::make_unique<MPI_Request[]>(num_procs);
+          {
+            //        std::lock_guard<hpx::lcos::local::mutex> lk(mt);
+            for (int src_rank = 0; src_rank < num_procs; ++src_rank) {
+              auto &req = recv_reqs[src_rank];
+              MPI_Irecv(recv_ptr + src_rank * num_elems, num_elems,
+                        tsgemm::get_mpi_type<scalar>(), src_rank, tag,
+                        comm_cart, &req);
+            }
+          }
 
-      // yield if not yet received
-      hpx::util::yield_while([num_procs, reqs = std::move(recv_reqs)] {
-        int flag;
-        MPI_Testall(num_procs, reqs.get(), &flag, MPI_STATUS_IGNORE);
-        return flag == 0;
-      });
+          // yield if not yet received
+          hpx::util::yield_while([num_procs, reqs = std::move(recv_reqs)] {
+            int flag;
+            MPI_Testall(num_procs, reqs.get(), &flag, MPI_STATUS_IGNORE);
+            return flag == 0;
+          });
 
-      //      int flag = 0;
-      //      while (flag == 0) {
-      //        MPI_Testall(num_procs, recv_reqs.get(), &flag,
-      //        MPI_STATUS_IGNORE); hpx::this_thread::yield();
-      //      }
+          //      int flag = 0;
+          //      while (flag == 0) {
+          //        MPI_Testall(num_procs, recv_reqs.get(), &flag,
+          //        MPI_STATUS_IGNORE); hpx::this_thread::yield();
+          //      }
 
-      // load
-      for (int src_rank = 0; src_rank < num_procs; ++src_rank) {
-        accumulate<scalar, 1>(prlen, pclen, recv_ptr + src_rank * num_elems,
-                              recv_ld, cfin_ptr, cfin_ld);
-      }
-    };
+          // load
+          for (int src_rank = 0; src_rank < num_procs; ++src_rank) {
+            accumulate<scalar, 1>(prlen, pclen, recv_ptr + src_rank * num_elems,
+                                  recv_ld, cfin_ptr, cfin_ld);
+          }
+        },
+        "recv/load");
 
-    auto load_fut =
-        hpx::dataflow(annotated_function(recv_and_load_func, "recv/load"));
-    comm_futures.push_back(std::move(load_fut));
+    auto load_fut = dataflow(executor, recv_and_load_func);
+
+    comm_futures.push_back(move(load_fut));
     rcv_offset += num_procs * num_elems;
     ++tag;
   };
@@ -250,12 +268,22 @@ void schedule_recv_and_load(
 //
 // All matrices are distributed in column-major order.
 //
-int tsgemm_main(hpx::program_options::variables_map &vm) {
+int hpx_main(hpx::program_options::variables_map &vm) {
   using scalar_t = std::complex<double>;
   using clock_t = std::chrono::high_resolution_clock;
   using seconds_t = std::chrono::duration<double>;
 
-  // printf("\n\nTOMA\n\n");
+#ifdef TSGEMM_USE_MPI_POOL
+  // use mpi pool
+  std::string pool_name = "mpi";
+#else
+  // use default pool for polling
+  std::string pool_name = "default";
+#endif
+  // an executor that can be used to place work on the MPI pool if it is enabled
+  auto mpi_executor = hpx::threads::executors::pool_executor(pool_name);
+
+  // hpx::threads::thread_priority_high
 
   int len_m = vm["len_m"].as<int>();
   int len_n = vm["len_n"].as<int>();
@@ -352,13 +380,14 @@ int tsgemm_main(hpx::program_options::variables_map &vm) {
     auto t1_gemm = clock_t::now();
 
     auto t0_send = clock_t::now();
-    schedule_offload_and_send(comm_cart, m_dim, n_dim, cini_buffer, send_buffer,
-                              gemm_futures, comm_futures);
+    schedule_offload_and_send(mpi_executor, comm_cart, m_dim, n_dim,
+                              cini_buffer, send_buffer, gemm_futures,
+                              comm_futures);
     auto t1_send = clock_t::now();
 
     auto t0_recv = clock_t::now();
-    schedule_recv_and_load(comm_cart, m_dim, n_dim, cfin_buffer, recv_buffer,
-                           comm_futures);
+    schedule_recv_and_load(mpi_executor, comm_cart, m_dim, n_dim, cfin_buffer,
+                           recv_buffer, comm_futures);
     auto t1_recv = clock_t::now();
 
     auto t0_wait = clock_t::now();
@@ -379,13 +408,13 @@ int tsgemm_main(hpx::program_options::variables_map &vm) {
   }
 
   // Simple check
-  //std::stringstream ss;
-  //using tsgemm::sum_global;
-  //ss << "cini sum = " << sum_global(comm_cart, cini_buffer) << '\n';
-  //ss << "send sum = " << sum_global(comm_cart, send_buffer) << '\n';
-  //ss << "recv sum = " << sum_global(comm_cart, recv_buffer) << '\n';
-  //ss << "cfin sum = " << sum_global(comm_cart, cfin_buffer) << '\n';
-  //if (rank == 0)
+  // std::stringstream ss;
+  // using tsgemm::sum_global;
+  // ss << "cini sum = " << sum_global(comm_cart, cini_buffer) << '\n';
+  // ss << "send sum = " << sum_global(comm_cart, send_buffer) << '\n';
+  // ss << "recv sum = " << sum_global(comm_cart, recv_buffer) << '\n';
+  // ss << "cfin sum = " << sum_global(comm_cart, cfin_buffer) << '\n';
+  // if (rank == 0)
   //  std::cout << ss.str() << '\n';
 
   return hpx::finalize();
@@ -399,7 +428,27 @@ int tsgemm_main(hpx::program_options::variables_map &vm) {
 //                       --blk_rows    32  --blk_cols    32
 //
 int main(int argc, char **argv) {
-  // MPI_THREAD_SERIALIZED
+
+  // init MPI
   auto mpi_handler = tsgemm::mpi_init{argc, argv, MPI_THREAD_MULTIPLE};
-  return hpx::init(tsgemm_main, tsgemm::init_desc(), argc, argv);
+
+  // declare options before creating resource partitioner
+  auto desc_cmdline = tsgemm::init_desc();
+
+#ifdef TSGEMM_USE_MPI_POOL
+  // NB.
+  // thread pools must be declared before starting the runtime
+
+  // Create resource partitioner
+  hpx::resource::partitioner rp(desc_cmdline, argc, argv);
+
+  // create a thread pool that is not "default" that we will use for MPI work
+  rp.create_thread_pool("mpi");
+
+  // add (enabled) PUs on the first core to it
+  rp.add_resource(rp.numa_domains()[0].cores()[0].pus(), "mpi");
+#endif
+
+  // init HPX
+  return hpx::init(desc_cmdline, argc, argv);
 }

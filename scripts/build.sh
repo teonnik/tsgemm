b354193bc93b93967ebb0da993c55321ccc948d7
Teodor Nikolov <teodor.nikolov22@gmail.com>
2020-02-18 10:11:14 +0100

    Add a build script for daint and laptop

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ---- input
+device=daint # laptop
+src_dir=$HOME/code/tsgemm
+build_dir=$HOME/build/tsgemm
+# ----
+
+# dependencies
+source $src_dir/scripts/$device/env.sh
+
+mkdir -p $build_dir
+cd $build_dir
+
+rm CMakeCache.txt
+rm -rf CMakeFiles
+
+cmake $src_dir \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D HPX_IGNORE_COMPILER_COMPATIBILITY=ON \
+  -D CMAKE_PREFIX_PATH=$hpx_dir
+
+make -j8
